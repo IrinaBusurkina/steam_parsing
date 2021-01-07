d8906f50912939d34ff0c68e0068b27953c4b6f4
IrinaBusurkina <74529283+IrinaBusurkina@users.noreply.github.com>
2021-01-07 16:43:08 +0300

    Update parse_steam_reviews.r

--- parse_steam_reviews.r
+++ parse_steam_reviews.r
@@ -1,3 +1,55 @@
+library(steamR)
+library(purrr)
+library(lubridate)
+library(dplyr)
+library(rjson)
+library(magrittr)
+library(tidyr)
+library(XML)
+library(httr)
+library(stringr)
+
+#get json with the appids from the steam api
+library(rjson)
+json_file <- "http://api.steampowered.com/ISteamApps/GetAppList/v2/"
+json_data <- fromJSON(f = json_file,unexpected.escape = "skip")
+applist <- json_data$applist$apps
+data <- applist %>% map_df(magrittr::extract,c("appid","name"))
+
+#join appids with game names
+# but firstly remove all punctuation 
+data$name <- tolower(data$name)
+data$name <- str_replace_all(data$name, "[[:punct:]]", "")
+data$name <- str_replace_all(data$name, "™", "")
+data$name <- str_replace_all(data$name, "®", "")
+data$name <- str_replace_all(data$name, "©", "")
+data$name <- str_trim(data$name, side = "both")
+data$name <- str_remove_all(data$name,"[:space:]{2,}")
+
+meta_2017$name <- tolower(meta_2017$name)
+meta_2017$name <- str_replace_all(meta_2017$name, "[[:punct:]]", "")
+meta_2017$name <- str_trim(meta_2017$name, side = "both")
+meta_2017$name <- str_remove_all(meta_2017$name,"[:space:]{2,}")
+
+#meta_2018$name <- str_replace_all(meta_2018$name, "guns gorecannoli 2", "guns gore and cannoli 2")
+#meta_2017$name <- str_replace_all(meta_2017$name, "divinity original sin ii", "divinity original sin 2")
+#meta_2017$name <- str_replace_all(meta_2017$name, "planescape tormentenhanced edition", "planescape torment enhanced edition")
+#data$name <- str_replace_all(data$name, "dark souls iiithe ringed city", "dark souls iii the ringed city")
+
+meta_2017 = meta_2017[,-9]
+#colnames(meta_2019)[9] = "appid"
+
+meta_2019 <- left_join(meta_2019, data, by = 'name')
+meta_2018 <- left_join(meta_2018, data, by = 'name')
+meta_2017 <- left_join(meta_2017, data, by = 'name')
+
+#find missing appids
+meta_2017$missing <- is.na(meta_2017$appid)
+missing <- meta_2018 %>% filter(missing == TRUE)
+meta_2017 <- meta_2017[!duplicated(meta_2017),]
+
+meta_2019 = na.omit(meta_2019)
+
 reviews_dfs = data.frame()
 
 for (i in games_id$appid){
